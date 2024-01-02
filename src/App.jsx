b0efaf3fc7adb5959e8ae571b50658bc71b3131e
ACXELL GONZALEZ NAVARRETE <acxell.gonzalez@fahorro.com.mx>
2024-01-02 10:28:17 -0600

    Base of Front

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <>
+      <h1>Alo?</h1>
+      <Button variant="contained"> DATA</Button>
+    </>
+  );
+}
+
+export default App;
